70d95268b6782001dde8117ab2bd3afdd3b48676
bavrillon <bastien.avrillon@etu.minesparis.psl.eu>
2025-01-31 12:12:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,24 @@ import ....
 
 
 reseau = Reseau()
-TIME = 0
+TIME = 0 # en jours
 
 class FilePrioriteEvenements:
     def __init__(self):
         self.file = []
     
-    def ajouter_evenement(self, destination, tps_trajet):
+    def ajouter_evenement(self, tps_trajet, destination):
         tps_arrivee = TIME + tps_trajet  
         heapq.heappush(self.file, (tps_arrivee, destination))
 
-    def recuperer_prochain_evenement(self):
-        return heapq.heappop(self.file)
+    def obtenir_prochain_evenement(self):
+        prochain_evenement = heapq.heappop(self.file)
+        delta_t = TIME
+        TIME = prochain_evenement[0]
+        delta_t = TIME - delta_t
+        for usine in reseau.usines :
+            usine.actualisation(delta_t)
+        for client in reseau.clients :
+            client.actualisation(delta_t)
+        
+        
