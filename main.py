8dc089f87c75a97de275b982fe048590b1b3611b
bavrillon <bastien.avrillon@etu.minesparis.psl.eu>
2025-01-31 12:03:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import heapq
+import ....
 
+
+reseau = Reseau()
+TIME = 0
+
+class FilePrioriteEvenements:
+    def __init__(self):
+        self.file = []
+    
+    def ajouter_evenement(self, destination, tps_trajet):
+        tps_arrivee = TIME + tps_trajet  
+        heapq.heappush(self.file, (tps_arrivee, destination))
+
+    def recuperer_prochain_evenement(self):
+        if self.file:
+            return heapq.heappop(self.file)
+        return None  
