aadf936c1961d248a2d2c230af096c58b9548a7b
bavrillon <bastien.avrillon@etu.minesparis.psl.eu>
2025-01-31 14:57:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,24 +8,3 @@ COST = 0
 
 while TIME <= 30 :
     reseau.file_events.obtenir_prochain_evenement()
-
-class FilePrioriteEvenements:
-    def __init__(self):
-        self.file = []
-    
-    def ajouter_evenement(self, tps_trajet, destination):
-        tps_arrivee = TIME + tps_trajet  
-        heapq.heappush(self.file, (tps_arrivee, destination))
-
-    def obtenir_prochain_evenement(self):
-        prochain_evenement = heapq.heappop(self.file)
-        COUT += ## Cout du trajet qui vient d'être effectué
-        delta_t = TIME
-        TIME = prochain_evenement[0]
-        delta_t = TIME - delta_t
-        for usine in reseau.usines :
-            usine.actualisation(delta_t)
-        for client in reseau.clients :
-            client.actualisation(delta_t)
-        
-        
