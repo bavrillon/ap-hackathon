d1b97a8fbbd8e6f1b68854f8d42fb9b2667ecdf8
Tristan Chariot <tristan.chariot@etu.minesparis.psl.eu>
2025-01-31 16:31:45 +0100

    ajout du chargement et déchargement du camion au bon endroit dans la pile de main.py

--- main.py
+++ main.py
@@ -16,16 +16,19 @@ while TIME <= 30 :
         usine.actualisation(delta_t)
     for client in reseau.clients :          # On actualise le stock des clients
         client.actualisation(delta_t)
-    # On redirige le camion :
+    
+    # On traite et redirige le camion :
     position_actuelle = destination
-    if destination.nature == usine :
+    if position_actuelle.nature == usine :
+        camion.recharge_camion(position_actuelle)
         prochain_client = camion.usine.clients[0]
         autonomie_min = 200000
         for client in camion.usine.clients :      
             if client.autonomy() <= autonomie_min :
                 prochain_client = client
         prochain_arret = prochain_client
-    elif destination.nature == client :
+    elif position_actuelle.nature == client :
+        camion.livraison_client(position_actuelle)
         prochain_arret = camion.usine
     tps_trajet = Camion.parametres_trajet(position_actuelle, prochain_arret)[0]
     reseau.file_events.ajouter_evenement(tps_trajet, position_actuelle, prochain_arret, camion.usine)
